2c2dd307b76696c9356ce633b7fd940dde1009d2
Daryl Collins <dcollins@polygon.technology>
2026-01-14 19:40:53 +0000

    chore: Require semicolons for consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   extends: 'standard',
   rules: {
     'space-before-function-paren': ['error', 'never'],
-    'no-underscore-dangle': 0
+    'no-underscore-dangle': 0,
+    semi: ['error', 'never'],
   },
   overrides: [
     {
       files: ['*test.js'],
       rules: {
-        'no-unused-expressions': 'off'
-      }
-    }
-  ]
-}
+        'no-unused-expressions': 'off',
+      },
+    },
+  ],
+};
