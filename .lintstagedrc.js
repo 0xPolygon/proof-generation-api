a54095d6cdab7647b7eae576d1f7a428b184ac21
Daryl Collins <dcollins@polygon.technology>
2026-01-16 13:51:38 +0000

    chore: Add commit hooks to enforce repository standards - Commit message structure must match conventional commits - Code linting and formatting is run pre-commit - Typechecking is run pre-commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,17 @@
+// This exists as a js file because .lintstagedrc w/ large #s of impacted files fails, but lintstagedrc.js works :)
+
+export default {
+  '*.{ts,cts,mts,tsx,js,cjs,mjs}': (files) => {
+    // const filteredFiles = files.filter((file) => !file.includes('exampleStr'));
+    return files.length > 0
+      ? [
+          `prettier --write ${files.join(' ')}`,
+          `eslint --fix ${files.join(' ')}`,
+        ]
+      : [];
+  },
+  '*.{json,md}': (files) => {
+    // const filteredFiles = files.filter((file) => !file.includes('exampleStr'));
+    return files.length > 0 ? `prettier --write ${files.join(' ')}` : [];
+  },
+};
